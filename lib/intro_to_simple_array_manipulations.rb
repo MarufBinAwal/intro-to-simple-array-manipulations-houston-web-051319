180d3afd716d7064686dce0358027b2a091f1baa
MARUF B AWAL <marufbinawal@gmail.com>
2019-05-12 16:17:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,6 +1,5 @@
  countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
     
-    
     updated_array = countries_in_western_africa.push("Niger")
     
     puts countries_in_western_africa
